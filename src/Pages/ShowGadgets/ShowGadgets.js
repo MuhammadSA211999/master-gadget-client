922e9b2686b9fe0cc04cf6b82a11d380dd50eb2d
Muhammad SA <MuhammadSA211999>
2022-05-01 19:59:55 +0600

    deliverupdate done

--- src/Pages/ShowGadgets/ShowGadgets.js
+++ src/Pages/ShowGadgets/ShowGadgets.js
@@ -1,15 +1,29 @@
+import axios from 'axios';
 import React from 'react';
 import { useState } from 'react'
+import toast from 'react-hot-toast';
 
 const ShowGadgets = ({ gadget }) => {
     const { name, price, image, supplier, quantity, _id } = gadget
-    // const [deliCount, setDeliCount] = useState()
-    // console.log(deliCount);
+    const [deliver, setDeliver] = useState(quantity)
+    if (quantity > deliver) {
+        toast.success('Successfully Deliver', { id: 'test' })
+    }
 
-    // const handleDeliver = (id) => {
-    //     setDeliCount(deliCount - 1)
+    (async () => {
+        const newUpdate = { deliver }
+        const url = `http://localhost:5000/deliverUpdate/${_id}`
+        try {
+            const { data } = await axios.put(url, newUpdate)
+            console.log(data);
 
-    // }
+        }
+        catch (error) {
+            console.log(error);
+
+        }
+    })()
+
     return (
         <li className="py-3 sm:py-4">
             <div className="flex items-center space-x-4">
@@ -26,9 +40,9 @@ const ShowGadgets = ({ gadget }) => {
                     <p className="text-sm text-gray-500 truncate dark:text-gray-400">
                         Stock: {quantity}
                     </p>
-                    {/* <button onClick={() => handleDeliver(_id)} className="text-sm text-gray-500 truncate dark:text-gray-400">
+                    <button onClick={() => setDeliver(deliver - 1)} className="text-sm text-gray-500 truncate dark:text-gray-400">
                         deliver
-                    </button> */}
+                    </button>
                 </div>
                 <div className="inline-flex items-center text-base font-semibold text-gray-900 dark:text-white">
                     $ {price}
