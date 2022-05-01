05b2dc7b1319bfb8f696bc691166981956e18815
Muhammad SA <MuhammadSA211999>
2022-05-02 03:55:06 +0600

    done

--- src/Pages/Blog/Blog.js
+++ src/Pages/Blog/Blog.js
@@ -1,6 +1,8 @@
 import React, { useState } from 'react';
 import { Accordion } from 'react-bootstrap';
 import toast, { Toaster } from 'react-hot-toast';
+import { Link } from 'react-router-dom';
+import sa from '../../Images/saleh.png'
 
 const Blog = () => {
 
@@ -22,7 +24,65 @@ const Blog = () => {
 
                     </Accordion.Header>
                     <Accordion.Body>
+                        <p className='text-xl font-medium'>USES OF MONGODB</p>
+                        <p>MongoDB represents the data as a collection of documents rather than tables related by foreign keys. This makes it possible for the varied types of data dealt over the internet to be stored decently and accessed in the web applications using Node. js.NoSQL databases like MongoDB are a good choice when your data is document-centric and doesn't fit well into the schema of a relational database, when you need to accommodate massive scale, when you are rapidly prototyping, and a few other use cases.MongoDB has become a popular choice of a highly scalable database and it is currently being used as the backend data store</p>
+                        <p className='text-xl font-medium'>USES OF NODEJS</p>
+                        <p>NODE JS is an asynchronous event-driven JavaScript runtime, Node.js is designed to build scalable network applications.Node. js is primarily used for non-blocking, event-driven servers, due to its single-threaded nature. It's used for traditional web sites and back-end API services, but was designed with real-time, push-based architectures in mind.
+                            <p>Backend for social media networking. </p>
+                            <p>Single-page application.</p>
+                            <p>Chat application.</p>
 
+                            <p>Data streaming.</p>
+                            <p>IoT application.</p>
+
+                        </p>
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>
+                        Diffrence between SQL and NoSQL
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        <p className='text-xl font-medium'>SQL DATABASE</p>
+                        <p>RELATIONAL DATABASE MANAGEMENT SYSTEM RDBMS</p>
+                        <p>These databases are best suited for complex queries</p>
+                        <p>Vertically Scalable</p>
+                        <p>SQL databases are better for multi-row transactions</p>
+                        <p>SQL databases use structured query language and have a predefined schema.</p>
+                        <p className='text-xl font-medium'>USES OF NODEJS</p>
+                        <p>Non-relational or distributed database system</p>
+                        <p> These databases are best suited for hierarchical data storage.</p>
+
+                        <p>Horizontally scalable</p>
+
+                        <p>NoSQL is better for unstructured data like documents or JSON.
+                        </p>
+                        <p>NoSQL databases have dynamic schemas for unstructured data.</p>
+
+
+                    </Accordion.Body>
+                </Accordion.Item>
+                <Accordion.Item eventKey="1">
+                    <Accordion.Header>
+                        Diffrence between JS and NODEJS
+                    </Accordion.Header>
+                    <Accordion.Body>
+                        <p className='text-xl font-medium'>JS</p>
+                        <p>Javascript is a programming language that is used for writing scripts on the website. </p>
+                        <p>These databases are best suited for complex queries</p>
+                        <p>Javascript can only be run in the browsers.It is basically used on the client-side.</p>
+                        <p>Javascript is used in frontend development.</p>
+                        <p>It is the upgraded version of ECMA script that uses Chrome’s V8 engine written in C++.
+                        </p>
+                        <p className='text-xl font-medium'>NODEJS</p>
+                        <p>We can run Javascript outside the browser with the help of NodeJS.</p>
+                        <p> It is mostly used on the server-side.</p>
+
+                        <p>We can run Javascript outside the browser with the help of NodeJS.</p>
+
+                        <p>Nodejs is used in server-side development.
+                        </p>
+                        <p>Nodejs is written in C, C++ and Javascript.</p>
                     </Accordion.Body>
                 </Accordion.Item>
             </Accordion>
